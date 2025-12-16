47c3d6b7e46473bdf96e3d61097bf4f846458a8e
Pascal <pascalchikezie64@gmamil.com>
2025-12-16 06:55:46 +0100

    Finished the footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,20 +4,24 @@ import Logo from '../assets/EstateViewLogo.png'
 function Footer(){
     return(
         <div className='FTDIV' id='contact'> 
-            <div className='IMGD'>
-                <img src={Logo} className='FTLOGO'/>
-            </div>
-            <div className='TXTAINP'>
-                <p className='SUBTXT'>Subscribe to Our Newsletter</p>
-                <div className='SUBD'> 
-                    <input type='text' className='NLI' placeholder='Your Email Address'/>
-                    <button className='SUBBTN'>Subscribe</button>
+            <div className='FLEX'>
+                <div className='IMGD'>
+                    <img src={Logo} className='FTLOGO'/>
+                </div>
+                <div className='TXTAINP'>
+                    <p className='SUBTXT'>Subscribe to Our Newsletter</p>
+                    <div className='SUBD'> 
+                        <input type='text' className='NLI' placeholder='Your Email Address'/>
+                        <button className='SUBBTN'>Subscribe</button>
+                    </div>
                 </div>
             </div>
             <div className='FooterLinks'>
+                <p className='LNK'>links:</p>
                 <a href="#homepage" className='FHOME'>Home</a>
                 <a href="#property-listings" className='FBUY'>Buy</a>
                 <a href="#property-listings" className='FRENT'>Rent</a>
+                <div></div>
                 <a href="#agent-listings" className='FAGENTS'>Agents</a> 
                 <a href="#guide" className='FGUIDE'>How It Works</a>  
                 <a href="" className='FSOC'>Socials</a>    
